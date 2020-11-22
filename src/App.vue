1deac249eba73a07103285518d42f1455ac481a8
Andrey <shock.work@mail.ru>
2020-11-22 14:39:34 +0300

    added layouts and routes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <div id="app">
+  <div id="app" class="wrapper">
+    <app-header/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+
+export default {
+  components: {AppHeader}
+}
+</script>
 
 <style>
 </style>
---
+++ src/views/Cart.vue
@@ -0,0 +1,21 @@
+<template>
+  <app-cart
+    v-if="false"
+  />
+  <app-cart-empty
+    v-else
+  />
+</template>
+
+<script>
+import AppCart from "@/components/AppCart";
+import AppCartEmpty from "@/components/AppCartEmpty";
+export default {
+name: "Cart",
+  components: {AppCartEmpty, AppCart}
+}
+</script>
+
+<style scoped>
+
+</style>
