62949b51317039e0749779f1de588d8c4d2f077c
Andrey <shock.work@mail.ru>
2021-02-13 11:59:13 +0300

    change btn to router-link

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -35,13 +35,13 @@
             <span> Сумма заказа: <b>900 ₽</b> </span>
           </div>
           <div class="cart__bottom-buttons">
-            <a href="/" class="button button--outline button--add go-back-btn">
+            <router-link to="/" class="button button--outline button--add go-back-btn">
               <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
 
               <span>Вернуться назад</span>
-            </a>
+            </router-link>
             <div class="button pay-btn">
               <span>Оплатить сейчас</span>
             </div>
