80041d46329a512d762c6b25a9f6d3168298c921
Andrey <shock.work@mail.ru>
2021-04-06 15:34:54 +0300

    prepare Categories component

---
+++ src/components/Categories.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="categories">
+    <ul>
+      <li :class="{ active: active === null }">Все</li>
+      <li
+        v-for="(name, index) in items"
+        :key="name"
+        :class="{ active: active === index }"
+      >
+        {{ name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Categories',
+  props: {
+    items: { type: Array, required: true },
+    active: { type: Number, required: true }
+  }
+}
+</script>
