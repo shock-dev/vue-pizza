c99aa3a2662214a3d0eeb45b854a1b2551aab3cd
Andrey <shock.work@mail.ru>
2021-04-06 17:39:10 +0300

    prepare store and resource of sorts added

---
+++ src/resources/sorts.js
@@ -0,0 +1,7 @@
+const sorts = [
+    { name: 'популярности', type: 'popular', order: 'desc' },
+    { name: 'цене', type: 'price', order: 'desc' },
+    { name: 'алфавит', type: 'name', order: 'asc' }
+]
+
+export default sorts
--- src/store/modules/filters.js
+++ src/store/modules/filters.js
@@ -2,17 +2,27 @@ export default {
     namespaced: true,
 
     state: {
-        category: null
+        category: null,
+        sortBy: {
+            name: 'популярности',
+            type: 'popular',
+            order: 'desc'
+        }
     },
 
     mutations: {
         setCategory(state, idx) {
             state.category = idx
+        },
+
+        setSortBy(state, val) {
+            state.sortBy = val
         }
     },
 
     getters: {
-        category: ({ category }) => category
+        category: ({ category }) => category,
+        sortBy: ({ sortBy }) => sortBy
     }
 
 }
