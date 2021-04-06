c896c5ac9ea0a77e2f33af43e5c42b64a37305d7
Andrey <shock.work@mail.ru>
2021-04-06 20:54:23 +0300

    transfer pizzas in PizzaBlock as props

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,50 +14,27 @@
     </div>
     <h2 class="content__title">Все пиццы</h2>
     <div class="content__items">
-      <div class="pizza-block">
-        <img
-            class="pizza-block__image"
-            src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-            alt="Pizza"
-        />
-        <h4 class="pizza-block__title">Чизбургер-пицца</h4>
-        <div class="pizza-block__selector">
-          <ul>
-            <li class="active">тонкое</li>
-            <li>традиционное</li>
-          </ul>
-          <ul>
-            <li class="active">26 см.</li>
-            <li>30 см.</li>
-            <li>40 см.</li>
-          </ul>
-        </div>
-        <div class="pizza-block__bottom">
-          <div class="pizza-block__price">от 395 ₽</div>
-          <div class="button button--outline button--add">
-            <svg
-                width="12"
-                height="12"
-                viewBox="0 0 12 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                  d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-                  fill="white"
-              />
-            </svg>
-            <span>Добавить</span>
-            <i>2</i>
-          </div>
-        </div>
-      </div>
+      <PizzaBlock
+        v-for="(item) in pizzas"
+        :key="item.id"
+        :name="item.name"
+        :price="item.price"
+        :imageUrl="item.imageUrl"
+        :category="item.category"
+        :rating="item.rating"
+        :types="item.types"
+        :sizes="item.sizes"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex'
+import {
+  mapGetters,
+  mapMutations,
+  mapActions
+} from 'vuex'
 
 // Components
 import Categories from '@/components/Categories'
@@ -66,27 +43,38 @@ import Sort from '@/components/Sort'
 // Resources
 import categories from '@/resources/categories'
 import sorts from '@/resources/sorts'
+import PizzaBlock from '@/components/PizzaBlock'
 
 export default {
   name: 'Home',
   components: {
     Categories,
-    Sort
+    Sort,
+    PizzaBlock
   },
   data: () => ({
     categories,
     sorts
   }),
+  mounted() {
+    this.fetchPizzas()
+  },
   computed: {
     ...mapGetters('filters', [
       'category',
       'sortBy'
+    ]),
+    ...mapGetters('pizza', [
+      'pizzas'
     ])
   },
   methods: {
     ...mapMutations('filters', [
       'setCategory',
       'setSortBy'
+    ]),
+    ...mapActions('pizza', [
+      'fetchPizzas'
     ])
   }
 }
