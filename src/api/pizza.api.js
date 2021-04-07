e6514b52c4b600b197752e29fa8308fb428de30e
Andrey <shock.work@mail.ru>
2021-04-07 13:07:14 +0300

    change pizza api [for filtering and sorting]

--- src/api/pizza.api.js
+++ src/api/pizza.api.js
@@ -2,7 +2,12 @@ import axios from 'axios'
 
 const API = 'http://localhost:5000'
 
-export const getPizzas = async () => {
-    const { data } = await axios.get(`${ API }/pizzas`)
+const prepareUrl = (category, sortBy) => {
+    const { type, order } = sortBy
+    return `${ category !== null ? `category=${ category }&` : '' }_sort=${ type }&_order=${ order }`
+}
+
+export const getPizzas = async ({ category, sortBy }) => {
+    const { data } = await axios.get(`${ API }/pizzas?${ prepareUrl(category, sortBy) }`)
     return data
 }
