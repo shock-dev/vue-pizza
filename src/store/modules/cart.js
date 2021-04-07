b4279272d66b489763c89e53d5fa2c59ccefbd1f
Andrey <shock.work@mail.ru>
2021-04-07 19:35:38 +0300

    logic for minus pizza from cart

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -8,31 +8,54 @@ export default {
     },
 
     mutations: {
-        setPizza(state, { id, pizza }) {
+        setPizza(state, pizza) {
+            const { id } = pizza
+
             if (!state.cart[id]) {
                 return state.cart[id] = [pizza]
             }
             state.cart[id] = [...state.cart[id], pizza]
         },
 
+        unsetPizza(state, id) {
+            state.cart[id].splice(state.cart[id].length - 1, 1)
+        },
+
         plusTotalPrice(state, price) {
             state.totalPrice += price
         },
 
+        minusTotalPrice(state, price) {
+            state.totalPrice -= price
+        },
+
         plusTotalCount(state) {
             state.totalCount++
+        },
+
+        minusTotalCount(state) {
+            state.totalCount--
         }
     },
 
     actions: {
-        plusPizza({ commit }, { id, pizza }) {
-            commit('setPizza', { id, pizza })
+        plusPizza({ commit }, pizza) {
+            commit('setPizza', pizza)
             commit('plusTotalPrice', pizza.price)
             commit('plusTotalCount')
+        },
+
+        minusPizza({ commit, state }, pizza) {
+            if (state.cart[pizza.id].length > 1) {
+                commit('unsetPizza', pizza.id)
+                commit('minusTotalPrice', pizza.price)
+                commit('minusTotalCount')
+            }
         }
     },
 
     getters: {
+        cart: ({ cart }) => cart,
         totalPrice: ({ totalPrice }) => totalPrice,
         totalCount: ({ totalCount }) => totalCount
     }
